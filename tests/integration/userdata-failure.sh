f93988e08c059dc9a5b9238d2680273e9d8aa366
Steffen Opel <sopel@opelbrothers.net>
2013-04-22 19:25:48 +0200

    Add failing UserData script fixtures

---
+++ tests/integration/userdata-failure.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# Template for an externally maintained script to be executed from the 
+# wrapper UserData script (also used as template 'do nothing' default)
+
+# BEGIN: insert custom code below this line, return non zero exit code on failure
+
+# return an epic failure
+return 42
+
+# END: insert custom code above this line
