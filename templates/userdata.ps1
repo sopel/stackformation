bf034e530a53a517ee14c8bca95c4d128c18ccb1
Steffen Opel <sopel@opelbrothers.net>
2013-04-22 08:16:32 +0200

    Add skeleton UserData scripts

---
+++ templates/userdata.ps1
@@ -0,0 +1,8 @@
+# Template for an externally maintained script to be executed from the 
+# wrapper UserData script (also used as template 'do nothing' default)
+
+# BEGIN: insert custom code below this line, return non zero exit code on failure
+
+exit $?
+
+# END: insert custom code above this line
