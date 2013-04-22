085ef461c7de9198a6b764fb0b1cfdc2897ebb86
Steffen Opel <sopel@opelbrothers.net>
2013-04-22 19:44:49 +0200

    Add succeeding UserData script fixtures

---
+++ tests/integration/userdata-success.ps1
@@ -0,0 +1,9 @@
+# Template for an externally maintained script to be executed from the 
+# wrapper UserData script (also used as template 'do nothing' default)
+
+# BEGIN: insert custom code below this line, return non zero exit code on failure
+
+# return an epic failure
+exit 42
+
+# END: insert custom code above this line
